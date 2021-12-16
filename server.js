463d48b26749315563628c541e9780415d91798a
playmakermz <sayaalif101@gmail.com>
2021-12-16 21:05:37 +0700

    set up express

---
+++ server.js
@@ -0,0 +1,8 @@
+let express = require('express');
+// Baris pertama, digunakan untuk akses library
+let app = express();
+// baris kedua digunakan untuk membuat instance dari express 
+
+app.listen(8000, function(){
+console.log('server is running')
+})
